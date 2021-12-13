6e0f45281192fb374361d0c5743767494ad08923
Elliot Alderson <elliotalderson4444@gmail.com>
2021-12-13 15:21:14 -0800

    just add the readme file and changed the logo name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     >
       <div className="space-y-4">
         <h1 className="text-2xl text-green-600 hover:text-green-700 cursor-pointer">
-          Zay Shop
+          Zay's Shop
         </h1>
         <p className="hover:text-gray-300 cursor-pointer hover:underline">
           123 Lahore
